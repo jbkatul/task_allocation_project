589ceb855c951fcf7829e95c46d6f6199060158f
NISHA KOTECHA <nishakotecha012@gmail.com>
2026-02-10 15:55:38 +0530

    Added demo greater-than function by Nisha Kotecha

--- iplapp/views.py
+++ iplapp/views.py
@@ -3,10 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 # by Nisha Kotecha
 
-a=10
-b=20    
-if a>b:
-    print("a is greater than b")    
+a = 10
+b = 20
+if a > b:
+    print("a is greater than b")
 else:
     print("b is greater than a")
-
