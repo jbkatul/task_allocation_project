639ce7eb219171a7ed86c5f910fc607b1ab19255
renu <renupatil54321@gmail.com>
2026-02-10 18:58:30 +0530

    added fibonacci feature

--- iplapp/views.py
+++ iplapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-# by harshada
+# by Renuka Patil
 
-def even_no(addtion):
-    if addition % 2 == 0:
-        return "Even"
-    else:
-        return "Odd"
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+
 
