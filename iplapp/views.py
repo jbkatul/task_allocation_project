81e7dc44b47de2e48610858edb73a2c5451cf44b
jbkatul <jbkatulraut@gmail.com>
2026-02-10 10:48:55 +0530

    Added login feature

--- iplapp/views.py
+++ iplapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# this is Dev 1 -> Atul sir's feature
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
