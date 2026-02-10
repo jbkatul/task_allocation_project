a17104b9e51969fa0f6abe6d5cb107e5d17f804a
Samrudhi1004 <samubartakke1004@gmail.com>
2026-02-10 11:19:02 +0530

    multiply and divide feature

--- iplapp/views.py
+++ iplapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# feature multiplication of two number 
+
+def multiply(a, b):
+    return a * b
+
+
+result = multiply(5, 3)
+print(result)  
+
+def divison(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Cannot divide by zero"
+result = divison(10, 2)
+print(result)
