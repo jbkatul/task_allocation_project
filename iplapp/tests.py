9aa5f5872ed9234b869e04057d81f2d244d309c6
DudhediyaYashasvi <dudhediyay@gmail.com>
2026-02-10 21:17:41 +0530

    conversion to uppercase

--- iplapp/tests.py
+++ iplapp/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 # yashasvi's code here
 def upper_case():
-    s = "python"
+    s = "programming is the art of breaking big problems into small victories"
     print(s.upper())
 
 upper_case()
