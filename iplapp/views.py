ebcc926c5ccd486552c90030ed8ae17510b8d808
harshadadeo2810 <harshadadeo84@gmail.com>
2026-02-10 10:45:21 +0530

    Updated file with latest changes

--- iplapp/views.py
+++ iplapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+# by harshada
+
+def even_no(addtion):
+    if addition % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
