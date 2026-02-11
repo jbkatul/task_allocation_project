0f980daf7f99f37ae160f68781a5cfe43a796834
Vaishnavi Pati <vaishnavipatil2924@gmail.com>
2026-02-11 23:47:21 +0530

    added list of IPL teams

--- iplapp/views.py
+++ iplapp/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
-# by harshada
 
-def even_no(addtion):
-    if addition % 2 == 0:
-        return "Even"
-    else:
-        return "Odd"
+# Function to return the list of IPL teams
+def get_ipl_teams():
+    """
+    Returns a list of IPL teams (as of 2024 season).
+    """
+    teams = [
+        "Chennai Super Kings",
+        "Mumbai Indians",
+        "Royal Challengers Bangalore",
+        "Kolkata Knight Riders",
+        "Sunrisers Hyderabad",
+        "Rajasthan Royals",
+        "Punjab Kings",
+        "Delhi Capitals",
+        "Lucknow Super Giants",
+        "Gujarat Titans"
+    ]
+    return teams
+
+
+# Function to display the teams in a formatted way
+def display_ipl_teams():
+    teams = get_ipl_teams()
+    print("List of IPL Teams:")
+    for idx, team in enumerate(teams, start=1):
+        print(f"{idx}. {team}")
 
