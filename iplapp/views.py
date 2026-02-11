4daac0f7a2d5c9959f831949aec8f5b9c3dba50c
saiprasad2610 <saiprasad26102004@gmail.com>
2026-02-11 08:13:54 +0530

    This is String Reverse Feature

--- iplapp/views.py
+++ iplapp/views.py
@@ -1,9 +1,9 @@
-from functools import reduce
 from django.shortcuts import render
 
 # Create your views here.
 # This is Saiprasad => 
 
-def MaxNumber(ls):
-    return reduce(lambda x,y: x if x>y else y, ls)
- 
+def reverseString(str):
+    return str[::-1]
+
+print(reverseString("saiprasad"))
